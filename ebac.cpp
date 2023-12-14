d3b8d28bbd3a68c990af2562e82c72e217a74cf0
ParkZuh <153832372+ParkZuh@users.noreply.github.com>
2023-12-14 10:21:38 -0300

    Projeto
    alteração de saída para o programa

--- ebac.cpp
+++ ebac.cpp
@@ -129,7 +129,8 @@ int main()
         printf("Escolha a opção desejada do menu:\n\n");
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
-        printf("\t3 - Deletar nomes\n\n"); 
+        printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n"); 
         printf("Opção: "); // fim do menu
      
         scanf ("%d", &opcao); // armazenando a escolha do usuario
@@ -148,6 +149,11 @@ int main()
          	
          	case 3:
             deletar(); //chamada de uma função
+         	break;
+         	
+         	case 4:
+         	printf("Obrigado por acessar o sistema!\n");
+         	return 0;
          	break;
          	
          	default:
